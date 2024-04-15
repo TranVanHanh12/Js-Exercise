5c0177d3445b07c71ee0c7f22900e86617aa81f5
TranVanHanh12 <kinhdoanhlamgiau1@gmail.com>
2024-04-15 16:36:37 +0700

    b2

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-// Viết hàm hoán đổi giá trị a, b, c với nhau
-const Shuffle = (items) => {
-    for (let i = items.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        let tmp = items[i];
-        items[i] = items[j];
-        items[j] = tmp;
+//  Viết hàm tính a mũ b  
+function tinhluythua(a, b) {
+    let luythua = 1;
+    for (var i = 0; i <= b; i++) {
+        luythua *= a;
     }
-    return items;
+    return luythua;
 }
-
+// Cách 2
+function tinhluythua(a, b) {
+    return Math.pow(a, b);
+}
