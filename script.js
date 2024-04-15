3f04b46ce6174d80e6c1afb554d5edaf9a15a996
TranVanHanh12 <kinhdoanhlamgiau1@gmail.com>
2024-04-15 17:07:15 +0700

    bt8

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-// Viết hàm tính tổng số lẻ trong mảng
-function demsole(arr) {
-    let tongso = 0;
-    for (var i = 0; i < arr.length; i++) {
-        if (arr[i] % 2 != 0) {
-            tongso += arr[i];
+// / Viết hàm kiểm tra 1 số có phải số nguyên tố hay ko
+
+function laSoNguyenTo(number) {
+    // Trường hợp đặc biệt: số nguyên tố phải lớn hơn 1
+    if (number <= 1) {
+        return false;
+    }
+
+    // Duyệt qua các số từ 2 đến căn bậc hai của number để kiểm tra tính chia hết
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+
+        if (number % i === 0) {
+            return false;
         }
     }
-    return tongso;
+
+    return true;
 }
