8f529e37f98fc799e0463a74e5085cd982a65291
TranVanHanh12 <kinhdoanhlamgiau1@gmail.com>
2024-04-15 17:19:28 +0700

    bt12

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-// Viết hàm tìm giá trị nhỏ nhất trong 1 mảng các số
-function timmin(arr) {
-    min = 0;
-    for (var i = 0; i < arr.length; i++) {
-        element = arr[i];
-        if (element < max) {
-            max = element;
-        }
+// Viết hàm tìm giá trị trung bình trong 1 mảng các số
+function average(arr) {
+    let ave = 0;
+    for (let i = 0; i < arr.length; i++) {
+        ave += arr[i];
     }
-    return min;
+    return (ave / arr.length)
+}
+
+// Viết hàm đảo ngược thứ tự của một mảng các số
+function daonguocmang(arr) {
+    let mangdaonguoc = [];
+    for (var i = arr.length - 1; i >= 0; i--) {
+        mangdaonguoc.push(arr[i]);
+    }
+    return mangdaonguoc;
 }
