482badb2a8a1a339d5e9af8b78397e2db236d362
TranVanHanh12 <kinhdoanhlamgiau1@gmail.com>
2024-04-15 16:43:51 +0700

    bt3

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-//  Viết hàm tính a mũ b  
-function tinhluythua(a, b) {
-    let luythua = 1;
-    for (var i = 0; i <= b; i++) {
-        luythua *= a;
+// Viết hàm kiểm tra tính đồng nhất chẵn lẻ của 2 số
+function check(a, b) {
+    if (a % 2 === 0 && b % 2 === 0 && a != 0 && b != 0) {
+        return ('toan la so chan');
+    } else if (a % 2 !== 0 && b % 2 !== 0 && a != 0 && b != 0) {
+        return ('toan la so le');
+    } else if (a % 2 !== 0 && b % 2 === 0 && a != 0 && b != 0) {
+        return ('1 le 1 chan');
+    } else if (a % 2 === 0 && b % 2 !== 0 && a != 0 && b != 0) {
+        console.log2('1 chan 1 le');
+    } else {
+        return '0 0'
     }
-    return luythua;
-}
-// Cách 2
-function tinhluythua(a, b) {
-    return Math.pow(a, b);
 }
