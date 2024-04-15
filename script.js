95804e144e04c1baa91d04756f17791d29d8f66a
TranVanHanh12 <kinhdoanhlamgiau1@gmail.com>
2024-04-15 17:16:30 +0700

    bt11

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// Viết hàm tìm giá trị lớn nhất trong 1 mảng các số
-function timmax(arr) {
-    max = 0;
+// Viết hàm tìm giá trị nhỏ nhất trong 1 mảng các số
+function timmin(arr) {
+    min = 0;
     for (var i = 0; i < arr.length; i++) {
         element = arr[i];
-        if (element > max) {
+        if (element < max) {
             max = element;
         }
     }
-    return max;
+    return min;
 }
