041228d065767f41d18d8d964146a701f8e803dc
TranVanHanh12 <kinhdoanhlamgiau1@gmail.com>
2024-04-15 17:22:58 +0700

    bt13

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-// Viết hàm tìm giá trị trung bình trong 1 mảng các số
-function average(arr) {
-    let ave = 0;
-    for (let i = 0; i < arr.length; i++) {
-        ave += arr[i];
-    }
-    return (ave / arr.length)
-}
-
 // Viết hàm đảo ngược thứ tự của một mảng các số
 function daonguocmang(arr) {
     let mangdaonguoc = [];
