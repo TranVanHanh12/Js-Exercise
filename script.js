baea7001d574ac0c46a6c7b7fb449033e76bab99
TranVanHanh12 <kinhdoanhlamgiau1@gmail.com>
2024-04-15 17:05:31 +0700

    bt7

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
-/  Viết hàm đếm số lượng số lẻ trong mảng
+// Viết hàm tính tổng số lẻ trong mảng
 function demsole(arr) {
-    let demso = 0;
+    let tongso = 0;
     for (var i = 0; i < arr.length; i++) {
         if (arr[i] % 2 != 0) {
-            demso += 1
+            tongso += arr[i];
         }
     }
-    return demso;
+    return tongso;
 }
-
-// cach 2
-const countEvenNumbers = (arr) => {
-
-    const evenNumbers = arr.filter(num => num % 2 != 0);
-
-    return evenNumbers.length;
-};
